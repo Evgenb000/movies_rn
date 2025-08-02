ac794fca5f21cc0ac384e9f8d3bff0b27897e34c
EvgenBabin <ebabin000@email.com>
2025-08-02 18:02:54 +0300

    home page, react native paper

--- movies/app/(tabs)/home.tsx
+++ movies/app/(tabs)/home.tsx
@@ -1,9 +1,11 @@
-import { Text, View } from "react-native";
+import { ScrollView, Text, TextInput, View } from "react-native";
 
 export default function Home() {
   return (
-    <View className="mt-12">
-      <Text>Home</Text>
+    <View className="flex-1 bg-red-200 pt-20">
+      <Text className="text-2xl text-center">Movies</Text>
+      <TextInput placeholder="Search" className="mt-4" />
+      <ScrollView></ScrollView>
     </View>
   );
 }
